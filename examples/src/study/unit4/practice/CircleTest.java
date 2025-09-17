b4f41619b991a14134be8f5221f36d1ba9396c99
yxt4200-svg <yxt4200@gmail.com>
2025-09-17 20:12:15 +0800

    class_examples

--- examples/src/study/unit4/practice/CircleTest.java
+++ examples/src/study/unit4/practice/CircleTest.java
@@ -9,29 +9,43 @@ public class CircleTest {
         c1.radius = 2.1;
 
         // 方法一
-//        double area = c1.findArea();
-//        System.out.println(area);
+        System.out.println(c1.findArea());
 
         // 方法二
-        c1.findArea();
+//        c1.findArea();
+
+        PassObjectTest test = new PassObjectTest();
+        Circle c =new Circle();
+        test.printAreas(c,5);
     }
 }
 
+class PassObjectTest {
+    public void printAreas(Circle c,int times){
+        System.out.println("Radius\t\tArea");
+
+        for(int i = 1;i <= times;i++){
+            c.radius = i;
+            System.out.println(c.radius + "\t\t" + c.findArea());
+
+        }
+    }
+}
 
 class Circle{
     // 属性
     double radius;
 
     // 求圆的面积的方法一
-//    public double findArea(){
-//        double area = Math.PI * radius * radius;
-//        return area;
-//     }
+    public double findArea(){
+        double area = Math.PI * radius * radius;
+        return area;
+     }
 
     // 求圆的面积的方法二
-    public void findArea(){
-        double area = Math.PI * radius * radius;
-        System.out.println("面积为：" + area);
-    }
+//    public void findArea(){
+//        double area = Math.PI * radius * radius;
+//        System.out.println("面积为：" + area);
+//    }
 
 }
